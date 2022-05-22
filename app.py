40019339adf50c32a9d9e65230cdd27b95f2c13a
martin.fleming <martin.flemin@externos-cl.cencosud.com>
2022-05-22 00:44:29 -0300

    docker-compose pull y docker-compose up -d en actions

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 	print("Connection OK")
-	return "Insta updateado (Puto el que lee)"
+	return "AAASHHEEEEEEE"
 
 
 if __name__ == "__main__":
